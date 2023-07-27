e777b3766164190d60c36ce1b547924639c1ce18
Oscar <oscarcr189@outlook.com>
2023-07-26 22:37:26 -0600

    modificando ts component y app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a Angular';
+
+  curso: string = 'Curso Spring con Angular';
+  desarrollador: string = 'Oscar Cervantes';
+
 }
