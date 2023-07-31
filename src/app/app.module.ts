266a35d87dc2dee921152a6ffd2e3a0fac068e2c
Oscar <oscarcr189@outlook.com>
2023-07-30 22:56:17 -0600

    ntegracion con Bostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,15 @@ import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
+import { RouterModule, Routes} from '@angular/router';
 
+
+const routes: Routes = [
+  { path: "", redirectTo: "/clientes", pathMatch: "full",},
+  { path: "directivas", component: DirectivaComponent},
+  {path:'clientes',component: ClientesComponent}, 
+
+];
 
 @NgModule({
   declarations: [
@@ -19,7 +27,8 @@ import { ClienteService } from './clientes/cliente.service';
   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
