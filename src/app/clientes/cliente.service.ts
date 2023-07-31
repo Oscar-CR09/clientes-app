67e8f3a07ba54b05ba7a9b2a7bc9dcee447bee34
Oscar <oscarcr189@outlook.com>
2023-07-30 22:25:39 -0600

    implementacion reactiva con la opcion observable

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json'; 
 import { Cliente } from './cliente';
-
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +10,9 @@ export class ClienteService {
 
   constructor() { }
 
-  getClientes(): Cliente[] { 
-    return  CLIENTES;
-
+  getClientes(): Observable<Cliente[]> { 
+    return  of(CLIENTES);
+    
    }
 
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -17,7 +17,10 @@ export class ClientesComponent implements OnInit{
     
   }
   ngOnInit() {
-    this.clientes = this.clienteService.getClientes();
-    
+    this.clienteService.getClientes().subscribe(
+      clientes => this.clientes=clientes
+      
+      );
+
   }
 }
