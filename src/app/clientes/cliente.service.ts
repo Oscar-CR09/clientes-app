21cfe62a193bf6f9b1ddd9d7e1870f888501bb20
Oscar <oscarcr189@outlook.com>
2023-08-04 22:33:57 -0600

    clientes

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json'; 
 import { Cliente } from './cliente';
 import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint: string= 'http://localhost:8080/api/cliente';
+  constructor(private http:HttpClient) { }
 
   getClientes(): Observable<Cliente[]> { 
-    return  of(CLIENTES);
-    
+    //return  of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
+    /*return this.http.get(this.urlEndPoint).pipe(
+    map( (response) => response as Cliente[])
+    );*/
+
    }
 
 }
