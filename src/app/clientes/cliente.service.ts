61456d80a2828d1419f3953eff8ea64a321c2a9b
Oscar <oscarcr189@outlook.com>
2023-08-12 18:09:07 -0600

    errores en el forn angular

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json'; 
 import { Cliente } from './cliente';
-import { of, Observable } from 'rxjs';
+import { of, Observable, throwError } from 'rxjs';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map,catchError } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ClienteService {
@@ -11,7 +14,7 @@ export class ClienteService {
   public urlEndPoint: string= 'http://localhost:8080/api/clientes';
 
   private httpHeaders= new HttpHeaders({'Content-Type': 'application/json'}); 
-  constructor(private http:HttpClient) { }
+  constructor(public http:HttpClient,public router: Router) { }
 
   getClientes(): Observable<Cliente[]> { 
     //return  of(CLIENTES);
@@ -27,7 +30,14 @@ export class ClienteService {
    }
 
    getCliente(id:number) : Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.urlEndPoint}/$ {id}`)
+    return this.http.get<Cliente>(`${this.urlEndPoint}/$ {id}`).pipe(
+      catchError(e=>{
+        this.router.navigate(['/clientes']);
+        console.error(e.error.mensaje);
+        Swal.fire('Error al editar',e.error.mensaje,'error');
+        return throwError(e) ;
+      }),
+    );
   }
 
   update(cliente: Cliente): Observable<Cliente> {
