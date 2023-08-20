7556d348165533adc5364a66e44ad3b89cfbddba
Oscar <oscarcr189@outlook.com>
2023-08-19 22:37:51 -0600

    manejo de fechas locale y pattersn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { RouterModule, Routes} from '@angular/router';
 import { HttpClientModule} from '@angular/common/http';
 import { FormComponent } from './clientes/form.component';
 import { FormsModule } from '@angular/forms'
+import { registerLocaleData} from '@angular/common';
+import localeES from '@angular/common/locales/es-MX';
+
+registerLocaleData(localeES,'es');
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full'},
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { formatDate, DatePipe} from '@angular/common';
+
 import { CLIENTES } from './clientes.json'; 
 import { Cliente } from './cliente';
 import { of, Observable, throwError } from 'rxjs';
@@ -27,8 +28,9 @@ export class ClienteService {
 
       return clientes.map(cliente => {
         cliente.nombre =cliente.nombre.toUpperCase();
+       
         //let datePipe = new DatePipe('en-US');
-        cliente.createAt = /*datePipe.transform(cliente.createAt, 'dd/MM/yyyy'); */ formatDate(cliente.createAt, 'dd/MM/yyyy','en-US');
+        cliente.createAt = /*datePipe.transform(cliente.createAt, 'dd/MM/yyyy'); */ formatDate(cliente.createAt, 'EEEE dd, MMMM yyyy','es-ES');
         return cliente;
       });
     }
